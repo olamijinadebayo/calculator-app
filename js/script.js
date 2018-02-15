cbc4537d510d3e264e7a8aaea68e64952fbb5783
olamijinadebayo <consultolamijin@gmail.com>
2018-02-15 09:48:35 +0300

    Modified index.html
    Added links to .js files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+
+
+// function for subtraction
+var subtract = function( num1, num2){
+   return num1 - num2;
+};
+var num1 = parseInt(prompt("enter a number:"));
+var num2 = parseInt(prompt("enter a number:"));
+var result = subtract (num1, num2);
+alert(result);
+
+// function for multiplication
+var multiplication = function( numb1, numb2){
+   return number1 * number2;
+};
+var numb1 = parseInt(prompt("enter a number:"));
+var numb2 = parseInt(prompt("enter a number:"));
+
+// function for division
+var divide = function (number1,number2) {
+  return number1 / number2;
+};
